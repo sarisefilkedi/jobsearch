dcf429defd119b8cdf599512d48399c01af4e32e
alexanderhovan <148468625+alexanderhovan@users.noreply.github.com>
2023-12-14 12:04:36 +0000

    Update draftsearch.py

--- jobsearchapp/draftsearch.py
+++ jobsearchapp/draftsearch.py
@@ -1,36 +1,46 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Flask, Blueprint, render_template, request
 import requests
 
+# Create a Flask application
+app = Flask(__name__)
+
+# Define your search Blueprint
 search_blueprint = Blueprint('search', __name__)
 
-@search_blueprint.route('/search', methods=['GET'])
-def search_jobs():
-    industry = request.args.get('industry')
-    country = request.args.get('country')
-    updated_since = request.args.get('updated_since')
+@search_blueprint.route('/search', methods=['GET', 'POST'])
+def search_companies():
+    companies = []
+    if request.method == 'GET':
+        industry = request.args.get('industry')
+        country = request.args.get('country')
+        last_updated_gte = request.args.get('last_updated_gte')
 
-    # CoreSignal API endpoint for LinkedIn company search with filters
-    api_url = 'https://api.coresignal.com/cdapi/v1/linkedin/company/search/filter'
-    headers = {
-        'Authorization': 'Bearer eyJhbGciOiJFZERTQSIsImtpZCI6ImM2MGY3NmIzLTBlOTYtMzM2NS1iN2UxLTdmYzBlYjUzYzk0OSJ9.eyJhdWQiOiJxbXVsIiwiZXhwIjoxNzM0MTA5MDE3LCJpYXQiOjE3MDI1NTIwNjUsI>
-    }
+        # Replace 'YOUR_API_KEY' with your actual API key
+        api_key = "eyJhbGciOiJFZERTQSIsImtpZCI6ImM2MGY3NmIzLTBlOTYtMzM2NS1iN2U1LTdmYzBlYjUzYzk0OSJ9.eyJhdWQiOiJxbXVsI..."
 
-    # Prepare the payload with filters
-    payload = {
-        "filter": {
-            "industry": industry,
-            "country": country,
-            "updated_at": {"gte": updated_since}
-        },
-        "size": 4  # Limiting the number of results to 4
-    }
+        # Replace 'API_ENDPOINT' with the actual API URL
+        api_url = 'https://api.coresignal.com/cdapi/v1/linkedin/company/search/filter'
 
-    response = requests.post(api_url, headers=headers, json=payload)
-    if response.status_code == 200:
-        companies_data = response.json()
-        top_companies = companies_data.get('data', [])  # Extract the data
-        return render_template('search_results.html', companies=top_companies)
-    else:
-        return jsonify({"error": "API request failed"}), response.status_code
+        # Set up headers with the API key
+        headers = {
+            'Authorization': f'Bearer {api_key}',
+            'Content-Type': 'application/json'
+        }
+
+        params = {
+            'industry': industry,
+            'country': country,
+            'last_updated_gte': last_updated_gte
+        }
+
+        response = requests.get(api_url, params=params, headers=headers)
+
+        if response.ok:
+            companies = response.json()  # Parse the response data
+
+    return render_template('search.html', companies=companies)
+
+# Register the search Blueprint
+app.register_blueprint(search_blueprint)
 
 
