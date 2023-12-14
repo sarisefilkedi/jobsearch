e6242ada3a5b5bff95e777ff2881a4456031e836
alexanderhovan <148468625+alexanderhovan@users.noreply.github.com>
2023-12-14 07:04:45 +0000

    Update login.py

--- jobsearchapp/login.py
+++ jobsearchapp/login.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+login_blueprint = Blueprint('login', __name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@login_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -10,14 +10,7 @@ def login():
         password = request.form['password']
         if (username == 'user1' and password == 'password1') or \
            (username == 'user2' and password == 'password2'):
-            return redirect(url_for('home'))
+            return redirect(url_for('main.home'))  # Assuming 'home' function is in the main module
         else:
             error = 'Invalid Credentials. Please try again.'
     return render_template('login.html', error=error)
-
-@app.route('/home')
-def home():
-    return 'Logged in successfully.'
-
-if __name__ == '__main__':
-    app.run(debug=True)
