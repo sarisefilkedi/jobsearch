19eb1f694a411120dc6ba57868457925ec26e719
alexanderhovan <148468625+alexanderhovan@users.noreply.github.com>
2023-12-14 07:55:02 +0000

    Update main.py

--- jobsearchapp/main.py
+++ jobsearchapp/main.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from login import login_blueprint
+from search import search_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(login_blueprint)
+    app.register_blueprint(login_blueprint, url_prefix='/')
+    app.register_blueprint(search_blueprint, url_prefix='/search')
     return app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
