fba06f69b412f933b072c95c5a3a3662690c6811
alexanderhovan <148468625+alexanderhovan@users.noreply.github.com>
2023-12-14 09:40:31 +0000

    Update main.py

--- jobsearchapp/main.py
+++ jobsearchapp/main.py
@@ -1,9 +1,16 @@
 from flask import Flask
-from login import login_blueprint
-from search import search_blueprint
+from models import db, User
+import os
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()  # Create database tables
+
     app.register_blueprint(login_blueprint, url_prefix='/')
     app.register_blueprint(search_blueprint, url_prefix='/search')
     return app
@@ -12,4 +19,5 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
 
