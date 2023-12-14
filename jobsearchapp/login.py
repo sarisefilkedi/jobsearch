b14285e5d97e63bf22cc986ca4486752aa0aae89
alexanderhovan <148468625+alexanderhovan@users.noreply.github.com>
2023-12-14 09:39:49 +0000

    Update login.py

--- jobsearchapp/login.py
+++ jobsearchapp/login.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 login_blueprint = Blueprint('login', __name__)
 
@@ -9,14 +11,25 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if (username == 'user1' and password == '1') or \
-           (username == 'user2' and password == '2'):
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
             return redirect(url_for('search.search_jobs'))
         else:
             error = 'Invalid Credentials. Please try again.'
     return render_template('login.html', error=error)
 
-@login_blueprint.route('/home')
-def home():
-    return 'Logged in successfully.'
+@login_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login.login'))
+    return render_template('register.html')
+
+
+
 
