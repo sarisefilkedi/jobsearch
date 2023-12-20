655b55aef96ac302f5c4894c3d7edd9575eefd99
alexanderhovan <148468625+alexanderhovan@users.noreply.github.com>
2023-12-20 16:17:31 +0000

    Update main.py

--- jobsearchapp/main.py
+++ jobsearchapp/main.py
@@ -1,24 +1,23 @@
 from flask import Flask
-from models import db, User
+from models import db
+from login import login_blueprint, create_admin_account
+from book_routes import book_blueprint  # Import the book routes
+# Assuming you have a blueprint for search functionality
+from search import search_blueprint
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    
+
     with app.app_context():
         db.create_all()  # Create database tables
+        create_admin_account()  # Create the admin account
 
-    # Import blueprints AFTER initializing db to avoid circular imports
-    from login import login_blueprint
-    from search import search_blueprint
-
-    # Register the search blueprint with '/search' prefix
-    app.register_blueprint(search_blueprint, url_prefix='/search')
-    
-    # Register the login blueprint with '/' prefix
     app.register_blueprint(login_blueprint, url_prefix='/')
+    app.register_blueprint(book_blueprint, url_prefix='/books')  # Register book routes
+    app.register_blueprint(search_blueprint, url_prefix='/search')  # Register search routes
 
     return app
 
