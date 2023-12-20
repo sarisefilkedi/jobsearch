73232ada73994e7050d01db0911d29f571ec9c00
alexanderhovan <148468625+alexanderhovan@users.noreply.github.com>
2023-12-20 15:27:41 +0000

    Update draftsearch.py

--- jobsearchapp/draftsearch.py
+++ jobsearchapp/draftsearch.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template, request
+from flask import Flask, Blueprint, render_template, request, jsonify, json
 import requests
 
 # Create a Flask application
