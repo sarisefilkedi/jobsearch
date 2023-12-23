b54cf6233dd8757c9e273b00b7e92544cd39aa77
alexanderhovan <148468625+alexanderhovan@users.noreply.github.com>
2023-12-23 16:22:36 +0000

    Update models.py

--- jobsearchapp/models.py
+++ jobsearchapp/models.py
@@ -6,6 +6,18 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    writer = db.Column(db.String(200), nullable=False)
+    genre = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    literature = db.Column(db.String(100), nullable=False)
+    language = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.name}>'
 
     def __repr__(self):
         return f'<User {self.username}>'
